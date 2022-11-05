18c8b1d42c8405017301f7ca64c788262905b1c4
mrtnandres <martinu615@gmail.com>
2022-11-04 23:18:51 -0300

    JSON

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-var numero = ["martin", "andres", "ulloa"];
 
-console.log(numero[0]+" "+numero[1] +" " +numero[2])
+
+
+async function obtenerDatos()
+{
+    const respuesta = await fetch('https://127.0.0.1:5500/registros.json');
+    const json = await respuesta.text();
+
+    console.log(json.nombre);
+}
+
+obtenerDatos();
